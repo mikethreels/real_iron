1cacb22a089af9a017cd826073a8aaf660b1905a
Michael Threels <michaelthreels@gmail.com>
2020-10-29 23:26:44 +0100

    fix: changed capitalization

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import 'phaser';
-import { config } from './config/config';
-import { GameScene } from './scenes/game_scene';
-import { GameOverScene } from './scenes/game_over_scene'
-import { BootScene } from './scenes/boot_scene';
-import { PreloaderScene } from './scenes/preloader_scene';
-import { TitleScene } from './scenes/title_scene';
-import { OptionsScene } from './scenes/options_scene';
-import { SubmitScore } from './scenes/submit_scene';
-import { CreditsScene } from './scenes/credits_scene';
-import { Model } from './model';
-import { Leaderboard } from './scenes/leader_board'
+import config from './config/config';
+import GameScene from './scenes/game_scene';
+import GameOverScene from './scenes/game_over_scene'
+import BootScene from './scenes/boot_scene';
+import PreloaderScene from './scenes/preloader_scene';
+import TitleScene from './scenes/title_scene';
+import OptionsScene from './scenes/options_scene';
+import SubmitScore from './scenes/submit_scene';
+import CreditsScene from './scenes/credits_scene';
+import Model from './model';
+import Leaderboard from './scenes/leader_board'
  
 class Game extends Phaser.Game {
   constructor () {
