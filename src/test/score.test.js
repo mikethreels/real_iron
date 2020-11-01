c57768463b0ba6a2a7746ad63272eea2e3514a23
Michael Threels <michaelthreels@gmail.com>
2020-11-01 22:56:01 +0100

    feat: worked out the tests making sure only 1 expect per unit test

--- src/test/score.test.js
+++ src/test/score.test.js
@@ -3,16 +3,20 @@ import getScores from './mocks/scoreMock';
 import '@babel/polyfill';
 
 
-test('Gets the names of the score and an object', async () => {
+test('Gets the names of the player', async () => {
   const scores = await getScores();
-
   expect(scores.user).toEqual('testUsername');
+});
+
+test('Gets an object', async () => {
   expect(typeof getScores()).toBe('object');
 });
 
-test('Gets the data from the Leaderboard', async () => {
+test('Gets score and checks if correct amount', async () => {
   const scores = await getScores();
-
   expect(scores.score).toEqual(1000);
+});
+
+test('Gets the data from the Leaderboard and checks if not undefined', async () => {
   expect(getScores()).not.toBe(undefined);
 });
